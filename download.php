ec7bee63d7cd043d1f0fead4ce5644ad65e6e0e4
DuckSoft <ducksoft@foxmail.com>
2017-08-19 10:12:59 +0800

    download.php: 去除有bug的URL检验

--- download.php
+++ download.php
@@ -17,19 +17,6 @@ $sw = new Swinggy([
                 return true;
             }
 
-            function validateURL($URL) {
-                $pattern_1 = "/^(http|https|ftp):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+.(com|org|net|dk|at|us|tv|info|uk|co.uk|biz|se)$)(:(\d+))?\/?/i";
-                $pattern_2 = "/^(www)((\.[A-Z0-9][A-Z0-9_-]*)+.(com|org|net|dk|at|us|tv|info|uk|co.uk|biz|se)$)(:(\d+))?\/?/i";
-                if(preg_match($pattern_1, $URL) || preg_match($pattern_2, $URL)){
-                    return true;
-                } else{
-                    return false;
-                }
-            }
-
-            if (!validateURL($_GET["url"])) {
-                return true;
-            }
             return false;
         },
         status_ok => function(){
