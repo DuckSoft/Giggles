9fe32eaa86c3579ebc9f78ae35c0344b7b030489
DuckSoft <ducksoft@foxmail.com>
2017-08-22 08:54:23 +0800

    shared.php: 实现Bootstrap panel代码的复用

--- shared.php
+++ shared.php
@@ -3,6 +3,16 @@
 const navbar_index = 0;
 const navbar_login = 1;
 
+function insert_panel($panel_type = "panel-default", $panel_title = "Title", $panel_body = "Body") {
+    echo "<div class=\"panel $panel_type\">
+<div class=\"panel-heading\">$panel_title</div>
+<div class=\"panel-body\">
+$panel_body
+</div>
+</div>";
+}
+
+
 function insert_footer() {
     echo <<<EOF
 <hr/>
